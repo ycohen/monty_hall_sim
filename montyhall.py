93b0466864841667fb0790ce48fb208a0ab5f31e
ycohen <yonatanmc@gmail.com>
2015-12-24 20:23:38 -0800

    Did you know that functions can return values? It's just like in math! We can use that fact to make the functions operate more independently, and to get rid of some dependencies on global variables.
    You should aim for functions not to have side effects, (like changing global variables,)
    unless it's really obvious that they should like when you're calling
    them specifically to manipulate a GUI.

--- montyhall.py
+++ montyhall.py
@@ -5,11 +5,8 @@
 #sets out the important variables
 import random
 global score
-global switchscore
+global switch_score
 global rounds
-global irounds
-score = 0
-switchscore = 0
 
 def pick_a_random_door():
     num = random.random()
@@ -21,52 +18,53 @@ def pick_a_random_door():
 def doors (): 
     car = pick_a_random_door()
     choose = pick_a_random_door()
-    global score
     if car == choose:
-        score = score+1
+        return 1
+    return 0
 
 #runs the switching-door scenario, +1 points for NOT choosing the right door
 def switch_doors ():
-    global switchscore
     car = pick_a_random_door()
     choose = pick_a_random_door()
     if car != choose:
-        switchscore = switchscore+1
+        return 1
+    return 0
 
 #asks the user to input the number of door-choosing rounds to run
 def start():
-    global rounds
-    global irounds
     rounds = raw_input("How many rounds?")
     rounds = int(rounds)
-    irounds = rounds
+    return rounds
     
 #calculates the percentage of "wins" for the both sets of rounds
 def calculation():
     global score
-    global switchscore
+    global switch_score
     global rounds
     print ("The Not-Switch Score is: ") + str(score)
-    print ("The Switching Score is: ") + str(switchscore)
+    print ("The Switching Score is: ") + str(switch_score)
     score = float(score)
-    switschscore = float(switchscore)
+    switsch_score = float(switch_score)
     rounds = float(rounds)
     points = (score*100/rounds)
-    switchpoints = (switchscore*100/rounds)
-    switchpoints = int(switchpoints)
+    switch_points = (switch_score*100/rounds)
+    switch_points = int(switch_points)
     points = int(points)
     print ("The probability for choosing the right door when you don't switch doors is: ") + str(points)
-    print ("The probability for choosing the right door when you do switch doors is: ") + str(switchpoints)
+    print ("The probability for choosing the right door when you do switch doors is: ") + str(switch_points)
 
 
 #run the program
+score = 0
+switch_score = 0
 
-start()
+rounds = start()
+irounds = rounds
 
 #runs the appropriate number of rounds
 while irounds > 0:
-    doors()
-    switch_doors()
+    score += doors()
+    switch_score += switch_doors()
     irounds = irounds-1
 
 #and gives the user the variables
